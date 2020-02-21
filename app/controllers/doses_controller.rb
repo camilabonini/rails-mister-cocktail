db285cef7881f3edbad293d04ab5fc1999cd4117
Camila Bonini <camilaa.bonini@yahoo.com.br>
2020-02-21 14:14:57 -0300

    this code has already made the controllers, model and view files

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,28 @@
+class DosesController < ApplicationController
+  before_action
+    def new
+      @doses = Dose.new
+    end
+
+    def create
+       @dose = Dose.new(dose_params)
+      if @dose.save
+        redirect_to dose_path(@cocktail)
+      else
+        render :new
+    end
+
+    def destroy
+      @dose = Dose.find(params[:id])
+      @dose.destroy
+    end
+
+    private
+    def dose_params
+      params.require(:dose).permit(:cocktail, :ingredient)
+    end
+
+    def set_dose
+      @dose = Dose.find(params[:id])
+    end
+end
